c088daf5c92b81a886ef3b122dcf304aa757ee58
Ian Jiang <ianjiang.ict@gmail.com>
2020-08-06 11:55:04 +0800

    slice-build.sh: Pass target platform parameters to compiler

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,10 +1,9 @@
 GEM5_DIR=../gem5
-#GEM5_DIR=../gem5-debug
 SIMPOINT_DIR=../Simpoint3.2
 GNU_PREFIX=riscv64-unknown-linux-gnu
 QEMU_BIN=../qemu/build/riscv64-softmmu/qemu-system-riscv64
+SPIKE_BIN=../spike-build-next/spike
 
 GEM5_MODE=debug
 ARCH=RISCV
 SIMPOINT_INTERVAL=1000
-TARGET_PLATFORM=qemu
--- scripts/slice-build.sh
+++ scripts/slice-build.sh
@@ -3,21 +3,35 @@
 scripts_path=$(cd "$(dirname $0)"; pwd)
 source $scripts_path/config.sh
 
-GCC=$GNU_PREFIX-gcc
-OBJDUMP=$GNU_PREFIX-objdump
-
-CFLAGS="-march=rv64g -mabi=lp64 -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles"
-
-LD_FILE=$scripts_path/../src/$TARGET_PLATFORM.ld
-BOOT_HEAD=$scripts_path/../src/$TARGET_PLATFORM.S
+target_platform="spike"
 
 if [ $# -gt 0 ]; then
 	slice_file=$1
 else
 	echo "Usage:"
-	echo "  slice-build.sh slice-file.S"
+	echo "  slice-build.sh slice-file.S [target-platform]"
+	echo "Example:"
+	echo "  slice-build.sh slice-file.S spike"
 	exit 1
 fi
+if [ $# -gt 1 ]; then
+	target_platform=$2
+fi
+
+if [ "$target_platform" == "spike" ]; then
+	FREE_MEM_BASE=0x40000000
+	FREE_MEM_SIZE=0x20000000
+fi
+
+GCC=$GNU_PREFIX-gcc
+OBJDUMP=$GNU_PREFIX-objdump
+
+CFLAGS="-march=rv64g -mabi=lp64 -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles"
+CFLAGS+=" -DFREE_MEM_BASE=$FREE_MEM_BASE"
+CFLAGS+=" -DFREE_MEM_SIZE=$FREE_MEM_SIZE"
+
+ld_file=$scripts_path/../src/$target_platform.ld
+boot_head=$scripts_path/../src/$target_platform.S
 
 if [ ! -f $slice_file ]; then
 	echo "Error: $slice_file not found"
@@ -30,7 +44,7 @@ if [ ! $? -eq 0 ]; then
 	exit 2
 fi
 
-$GCC $CFLAGS -T$LD_FILE $BOOT_HEAD $slice_file -o $slice_file.elf
+$GCC $CFLAGS -T$ld_file $boot_head $slice_file -o $slice_file.elf
 $OBJDUMP -h $slice_file.elf
 $OBJDUMP -D $slice_file.elf > $slice_file.elf.S
 
