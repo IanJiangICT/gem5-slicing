7727a6cd73772b1f5da19f64bc7c42a9e50ec4b6
Ian Jiang <ianjiang.ict@gmail.com>
2020-08-09 20:02:00 +0800

    slice-run.sh: Support spike

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,8 +2,9 @@ GEM5_DIR=../gem5
 SIMPOINT_DIR=../Simpoint3.2
 GNU_PREFIX=riscv64-unknown-linux-gnu
 QEMU_BIN=../qemu/build/riscv64-softmmu/qemu-system-riscv64
-SPIKE_BIN=../spike-build-next/spike
+SPIKE_BIN=../spike-build-dev/spike
 
 GEM5_MODE=debug
 ARCH=RISCV
 SIMPOINT_INTERVAL=1000
+SIMPOINT_INTERVAL=5000
--- scripts/slice-run.sh
+++ scripts/slice-run.sh
@@ -3,14 +3,19 @@
 scripts_path=$(cd "$(dirname $0)"; pwd)
 source $scripts_path/config.sh
 
-
+target_platform="spike"
 
 if [ $# -gt 0 ]; then
 	slice_elf=$1
 else
 	echo "Usage:"
-	echo "  slice-run.sh slice.elf"
+	echo "  slice-run.sh slice.elf [target-platform]"
+	echo "Example:"
+	echo "  slice-run.sh slice.elf spike"
 	exit 1
+fi
+if [ $# -gt 1 ]; then
+	target_platform=$2
 fi
 
 if [ ! -f $slice_elf ]; then
@@ -18,17 +23,32 @@ if [ ! -f $slice_elf ]; then
 	exit 2
 fi
 
-which $QEMU_BIN >> /dev/null
-if [ ! $? -eq 0 ]; then
-	echo "Error: $QEMU_BIN not found"
-	exit 2
+instr_count=1000
+
+if [ "$target_platform" == "spike" ]; then
+	which $SPIKE_BIN >> /dev/null
+	if [ ! $? -eq 0 ]; then
+		echo "Error: $SPIKE_BIN not found"
+		exit 2
+	fi
+	mr_list=0x92000:0x28000,0x40000000:0x80000000
+	other_options=--log-commits
+	other_options=-l
+	$SPIKE_BIN --inst-toplimit=$instr_count -m$mr_list $other_options $slice_elf
+elif [ "$target_platform" == "qemu" ]; then
+	which $QEMU_BIN >> /dev/null
+	if [ ! $? -eq 0 ]; then
+		echo "Error: $QEMU_BIN not found"
+		exit 2
+	fi
+
+	$QEMU_BIN -machine help | grep multico >> /dev/null
+	if [ ! $? -eq 0 ]; then
+		echo "Error: QEMU must support multico machine"
+		exit 2
+	fi
+
+	$QEMU_BIN -nographic -machine multico -m 4G \
+			  -kernel $slice_elf
 fi
 
-$QEMU_BIN -machine help | grep multico >> /dev/null
-if [ ! $? -eq 0 ]; then
-	echo "Error: QEMU must support multico machine"
-	exit 2
-fi
-
-$QEMU_BIN -nographic -machine multico -m 4G \
-          -kernel $slice_elf
