24d28a47ddf6da4ec077adb51f8f2bc93f722d2c
Ian Jiang <ianjiang.ict@gmail.com>
2020-08-10 16:13:47 +0800

    slice-run.sh: Check running result
    1. Check length of running log
    2. Get last PC in running log
    3. Search last PC in ELF objdump file

--- scripts/slice-run.sh
+++ scripts/slice-run.sh
@@ -61,6 +61,24 @@ if [ "$target_platform" == "spike" ]; then
 	echo "Inst count = " $inst_cnt_boot " + " $inst_cnt_init " + " $inst_cnt_slice " Total " $inst_cnt_total
 	echo "Log        = " $log_file
 	$SPIKE_BIN --inst-toplimit=$inst_cnt_total -m$mr_list $other_options $slice_elf > $log_file 2>&1
+
+	log_lines=`wc -l $log_file | cut -d ' ' -f 1`
+	if [ "$log_lines" -lt "$inst_cnt_total" ]; then
+		echo "Error: Not enough running log. Line count $log_lines"
+		exit 1
+	fi
+	pc_last=`tail -n 3 $log_file | head -n 1 | cut -d 'x' -f 2 | cut -b 9-16`
+	if [ ! ${#pc_last} -eq 8 ]; then
+		echo "Error: Invalid last PC $pc_last"
+		exit 1
+	fi
+	grep $pc_last $slice_elf.S > /dev/null 2>&1
+	if [ ! $? -eq 0 ]; then
+		echo "Error: Last PC $pc_last not in $slice_elf.S"
+		exit 1
+	fi
+	echo "Success"
+	exit 0
 elif [ "$target_platform" == "qemu" ]; then
 	which $QEMU_BIN >> /dev/null
 	if [ ! $? -eq 0 ]; then
